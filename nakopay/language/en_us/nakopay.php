4e8391ee0b0d85171da13e93f12245e2e745f3a5
NakoPay Ops <ops@nakopay.com>
2026-05-17 00:22:33 -0400

    chore: sync from monorepo

---
+++ nakopay/language/en_us/nakopay.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Nakopay.name'] = 'NakoPay';
+$lang['Nakopay.description'] = 'Accept cryptocurrency payments via NakoPay.';
+$lang['Nakopay.api_key'] = 'API Key';
+$lang['Nakopay.api_key.note'] = 'Your sk_live_* or sk_test_* key from nakopay.com/dashboard/api-keys';
+$lang['Nakopay.webhook_secret'] = 'Webhook Secret';
+$lang['Nakopay.webhook_secret.note'] = 'HMAC secret for verifying webhook signatures';
+$lang['Nakopay.test_mode'] = 'Test Mode';
+$lang['Nakopay.accepted_currencies'] = 'Accepted Currencies';
+$lang['Nakopay.!error.api_key.valid'] = 'API key must start with sk_live_ or sk_test_';
+$lang['Nakopay.!error.webhook.signature'] = 'Invalid webhook signature';
